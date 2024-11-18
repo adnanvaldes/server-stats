993e75df20eb52e6baf92221c6d4b6a9d5a6ea37
Adnan Valdes <github@terminus.earth>
2024-11-18 13:40:20 -0800

    Add top5 procs functions

--- server-stats.sh
+++ server-stats.sh
@@ -2,9 +2,9 @@
 
 totalCPU () {
     # diving by cores to get total CPU utilization
-    ps -eo pcpu | awk -v cores=$(nproc) '{total += $1} END {print total / cores}'
-}
+    ps -eo pcpu | awk -v cores=$(nproc) '{total += $1} END {print sprintf("CPU Load: %.1f", total / cores)"%"}'
 
+}
 
 # Total memory usage (Free vs Used including percentage)
 totalMem () {
@@ -19,18 +19,25 @@ totalMem () {
               "\nCache:", $6, percentage($6, $2)
     }'
 }
-# Total disk usage (Free vs Used including percentage)
 
+# Total disk usage (Free vs Used including percentage)
 diskUsage () {
     df -h --total | grep "/" -w | awk '{split($1, arr, "/"); print "Device:", arr[length(arr)], "\nUsage:", $2, $3, $4, $5}'
 }
 
 # Top 5 processes by CPU usage
+topCPUProcs () {
+    ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head -6
+}
 # Top 5 processes by memory usage
-
+topMemProcs () {
+    ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head -6
+}
 
 # Stretch goal: Feel free to optionally add more stats such as os version, uptime, load average, logged in users, failed login attempts etc.
 
 totalCPU
 totalMem
 diskUsage
+topCPUProcs
+topMemProcs
